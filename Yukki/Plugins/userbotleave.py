5b9f192b56b151b9dd87fd3f004fba6d8f2e23d4
DoniRamadhan <95742544+Donigmg@users.noreply.github.com>
2022-04-14 05:17:38 +0700

    Update userbotleave.py

--- Yukki/Plugins/userbotleave.py
+++ Yukki/Plugins/userbotleave.py
@@ -7,11 +7,11 @@ from Yukki import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5, SUDOER
 
 @app.on_message(filters.command("joinall") & filters.user(SUDOERS))
 async def join(client, message):
-  await ASS_CLI_1.join_chat("-1001608959025")
-  await ASS_CLI_2.join_chat("-1001608959025")
-  await ASS_CLI_3.join_chat("-1001608959025")
-  await ASS_CLI_4.join_chat("-1001608959025")
-  await ASS_CLI_5.join_chat("-1001608959025")
+  await ASS_CLI_1.join_chat("-1001616236548")
+  await ASS_CLI_2.join_chat("-1001616236548")
+  await ASS_CLI_3.join_chat("-1001616236548")
+  await ASS_CLI_4.join_chat("-1001616236548")
+  await ASS_CLI_5.join_chat("-1001616236548")
 
 @ASS_CLI_1.on_message(filters.command("userbotleaveall") & filters.user(SUDOERS))
 async def bye(client, message):
